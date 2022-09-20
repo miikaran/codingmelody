06ed39be998d9b575b576158aa9d6f25db7f2eb0
miikaran <42628@edu.raseko.fi>
2022-09-20 22:17:31 +0300

    fix bg config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       'inter': ['Inter'],
     },
     backgroundImage: {
-      'mainpage': "url('https://steamuserimages-a.akamaihd.net/ugc/945094571616867748/680E4979CC75A2310664E0883F3F3FC6CBECA3BE/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false')",
+      'mainpage': "url('../src/assets/mainpagebg.gif')",
     }
   },
   plugins: [],
