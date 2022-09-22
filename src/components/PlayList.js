3511e345415b23bcf69f62047ca3507e2455c062
miikaran <42628@edu.raseko.fi>
2022-09-22 22:37:50 +0300

    add CreatePlaylist + FilterPlaylist
    Now you can create new playlists & filter music inside them :)

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -9,6 +9,7 @@ import { IoIosPlay, IoIosRemoveCircleOutline } from 'react-icons/io'
 import { MdPlaylistAdd } from 'react-icons/md'
 import { AiOutlineClose } from 'react-icons/ai'
 import { GiLoveSong } from 'react-icons/gi'
+import { RiPlayListFill, RiPlayListAddLine } from 'react-icons/ri'
 
 export default function PlayList(props) {
 
@@ -19,23 +20,28 @@ export default function PlayList(props) {
     const [search, setSearch] = useState('');
     const [searchedItems, setSearchedItems] = useState([]);
     const [addingItem, setAddingItem] = useState(false);
+    const [playlistName, setPlaylistName] = useState([]);
+    const [addingPlaylist, setAddingPlaylist] = useState(false);
     const [itemAdded, setItemAdded] = useState(false);
     const [successfull, setSuccessfull] = useState('');
     const [itemDeleted, setItemDeleted] = useState(false);
     const [deleted, setDeleted] = useState('');
     const [noMatches, setNoMatches] = useState(false);
+    const [playlists, setPlaylists] = useState([]);
     
     /*READS USERS PLAYLIST
     DATA FROM FIREBASE*/
     useEffect(() =>{
         auth.onAuthStateChanged((user) => {         
             if(user){
-                //onValue listens for events & changes & updates them.
-                onValue(ref(db, `/${auth.currentUser.uid}`), snapshot => {
+                //onValue listens for events/changes & updates them.
+                onValue(ref(db, `/${auth.currentUser.uid}/${playlistName}`), snapshot => {
                     setItems([])
+                    setPlaylists([])
                     const data = snapshot.val()
                     if(data !== null){
                         Object.values(data).map((items) => {
+                            setPlaylists((playlists) => [...playlists, items])
                             setItems((oldArray) => [...oldArray, items])             
                         })
                     }
@@ -56,6 +62,10 @@ export default function PlayList(props) {
         setName(event.target.value);
     };
 
+    const handlePlaylistNameInputChange = (event) => {
+        setPlaylistName(event.target.value);
+    };
+
 
     /*HANDLE ADDING
     PLAYLIST ITEMS*/
@@ -69,6 +79,18 @@ export default function PlayList(props) {
             writeToFireBase();
             setUrl('');
             setName('');
+        }
+    };
+
+
+    const handlePlaylistSubmit = (event) => { 
+
+        event.preventDefault();
+        if (playlistName !== ''){
+            writeNewPlaylistToFireBase();
+        }
+        else{
+            displayErrorOnAdding();
         }
     };
 
@@ -118,11 +140,21 @@ export default function PlayList(props) {
     /*WRITE PLAYLIST ITEMS TO 
     FIREBASE CURRENT USER*/
      const writeToFireBase = () => {
-        
+
         const userId = uid();
-        set(ref(db, `${auth.currentUser.uid}/${userId}`), {
+        set(ref(db, `${auth.currentUser.uid}/${playlistName}/${userId}`), {
             PlaylistItem: url, name,
             userId: userId,
+        })
+        setItemAdded(true);
+        itemAddedSuccessfully();
+    }
+
+
+    const writeNewPlaylistToFireBase = () => {
+        
+        set(ref(db, `${auth.currentUser.uid}/${playlistName}`), {
+            PlaylistName: playlistName,
         })
         setItemAdded(true);
         itemAddedSuccessfully();
@@ -166,6 +198,20 @@ export default function PlayList(props) {
     }
 
 
+    const filterPlaylist = (playlist) => {
+
+        setPlaylistName(playlist)
+
+        onValue(ref(db, `/${auth.currentUser.uid}/${playlist}`), snapshot => {
+            setItems([])
+            const data = snapshot.val()
+            Object.values(data).map((items) => {
+                setItems((oldArray) => [...oldArray, items])             
+            })   
+        })
+    }
+
+
     return(
 
         <div className="text-white bg-opacity-20 sm:px-12 space-y-10 mt-10">
@@ -173,6 +219,9 @@ export default function PlayList(props) {
             <div className="flex flex-col sm:flex-row justify-center mx-2 sm:mx-0 space-x-0 space-y-6 sm:space-y-0 sm:space-x-3">
                 <button onClick={() => setAddingItem(true)}className="flex text-white text-lg bg-indigo-500 bg-opacity-80 px-4 py-3 rounded-sm font-bold hover:bg-indigo-500 transition duration-200">
                         ADD SONGS<GiLoveSong className="text-2xl mx-2" />           
+                </button>
+                <button onClick={() => setAddingPlaylist(true)}className="flex text-white text-lg bg-indigo-500 bg-opacity-80 px-4 py-3 rounded-sm font-bold hover:bg-indigo-500 transition duration-200">
+                        CREATE PLAYLIST<RiPlayListAddLine className="text-2xl mx-2" />           
                 </button>
                 <input onChange={(e) => searchItems(e.target.value)} type="text" className="pl-3 pr-20 py-2 rounded-sm text-lg bg-gray-900 bg-opacity-60 border" placeholder="Search Your Songs..."/>                 
             </div>
@@ -195,6 +244,22 @@ export default function PlayList(props) {
             ): null}
 
 
+            { addingPlaylist ? (
+
+            <div className="flex flex-col justify-center lg:mx-60 bg-gray-900 px-7 py-6 border">
+                <button onClick={() => setAddingPlaylist(false)} className="border py-2 sm:py-1 w-8 px-2 border-red-500 bg-red-500"><AiOutlineClose className="text-xs" /></button>
+                <span className="text-2xl font-bold text-center">CREATE PLAYLIST</span>
+                <form onSubmit={handlePlaylistSubmit} className="md:space-x-2 rounded-sm justify-center py-2 space-y-3 sm:space-y-0 flex flex-col sm:flex-row">
+                    <input onChange={handlePlaylistNameInputChange} value={playlistName} type="text" className="pl-3 pr-4 py-2 sm:py-0 rounded-sm bg-gray-900 bg-opacity-60 border" placeholder="Enter Name Here.."/>                       
+                    <button type="submit" className="flex text-white md:mx-2 bg-indigo-400 px-4 py-3 rounded-sm font-bold hover:bg-indigo-500 transition duration-200">
+                        CREATE PLAYLIST<MdPlaylistAdd className="text-2xl mx-2" />           
+                    </button>
+                </form>
+            </div>
+            ): null}
+
+
+
             {itemAdded ? (
                     <div className="ml-20">
                         <p className="text-green-300 font-bold mx-1">{successfull}</p>
@@ -209,6 +274,15 @@ export default function PlayList(props) {
             ): null}
 
             <div className="block text-red-400 mx-1 font-bold">{error}</div>
+
+            
+            <div className="flex justify-center space-x-3">
+                <select onChange={(e) => filterPlaylist(e.target.value)} className="text-sm border font-medium rounded-sm text-white px-4 py-1 mt-1 bg-gray-900">
+                    {playlists.map((data) => (
+                        <option value={data.PlaylistName}>{data.PlaylistName}</option>
+                    ))}
+                </select>
+            </div>
 
 
             {noMatches ? (
@@ -242,6 +316,7 @@ export default function PlayList(props) {
                             return(
 
                                 <button onClick={() => passSongUrlToParent((data.PlaylistItem), (data.name))} className="bg-opacity-30 border border-white border-opacity-5 space-x-1 sm:space-x-4 flex bg-gray-900 px-5 py-6 sm:py-3 rounded-sm">
+        
                                     <div className="w-10 sm:w-12">
                                         <img alt="Thumbnail" src="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/146301004/original/265af19662a8925f79d1e3d2daff3c5c8277ee3c/create-album-covers-and-song-covers-for-cheap.jpg" />
                                     </div>
