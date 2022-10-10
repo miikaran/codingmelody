1db849ff0abc9e5c60ba23e94001ba26e20d8274
miikaran <42628@edu.raseko.fi>
2022-10-10 14:37:38 +0300

    fix dead links

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -15,10 +15,10 @@ export default function Footer() {
                             SIGNUP
                         </Link>
                         <a href="https://github.com/miikaran/codingmelody">SOURCE</a>
-                        <a href="https://github.com/miikaran/codingmelody">DEMO</a>
+                        <a href="https://drive.google.com/file/d/1GA1O1sjhS6nhi8gIHElrsT7x4ejdmoUT/view?usp=sharing" target="_blank" >DEMO</a>
                     </li>
                     <div className="text-white text-center">
-                        2022 @CodingMelody
+                        2022 @<Link to="/" className="underline">CodingMelody</Link>
                     </div>
                 </ul>
             </div>
